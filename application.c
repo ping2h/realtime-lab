7a6329d26e68dcb9366ca9bcdf92919b8f1411d5
Ping Zheng <liaoupup@gmail.com>
2023-04-07 22:58:10 +0200

    with 2 nodes, it can work

--- application.c
+++ application.c
@@ -12,6 +12,9 @@
 #define MOMENTARY 1
 #define CONDUCTOER 1
 #define MUSICIAN 2
+#define OPCODE 0
+#define RECEIVER 1
+
 
 
 
@@ -39,6 +42,8 @@ typedef struct {
     int index;
     char buffer[50];
     int mod;
+    int numOfNodes;   // 
+    int Id;
 } App;
 
 typedef struct {
@@ -62,6 +67,7 @@ typedef struct {
     int start;
     
 } MusicPlayer;
+
 
 typedef struct {
     Object super;
@@ -98,6 +104,10 @@ bool tempobool = false;
 ///////////////////////////////////////////////////////////
 void reader(App*, int);
 void receiver(App*, int);
+void newrec(App*, int);
+// initally broadcast
+void pitchSelfRPC(int);
+int getId(App*, int);  //get the ID of the node
 
 void press(Button*, int);
 void check1Sec(Button*, int);
@@ -123,6 +133,11 @@ void changeTempo(MusicPlayer*, int);
 int  checkStart(MusicPlayer*, int);
 void start(MusicPlayer*, int);
 void stop(MusicPlayer*, int);
+void play1Note(MusicPlayer*, int);
+void stopAndSend(MusicPlayer*, int);
+void play1NoteRPC(int, int);
+void startDS(MusicPlayer*, int);
+void startRPC(int);
 
 void blink(LED*, int);
 void changeLed(LED*, int);
@@ -132,10 +147,10 @@ void stopled(LED*, int);
 
 
 ///////////////////////////////////////////////////////////
-App app = { initObject(), 0 , {}, CONDUCTOER};
+App app = { initObject(), 0 , {}, CONDUCTOER, 1, 0}; // id 0, H priority
 Serial sci0 = initSerial(SCI_PORT0, &app, reader);
 Semaphore muteVolumeSem = initSemaphore(1);       // lock the tg when is muted
-Can can0 = initCan(CAN_PORT0, &app, receiver); 
+Can can0 = initCan(CAN_PORT0, &app, newrec); 
 Button bt = {initObject(), initTimer(), 0, 0, MOMENTARY, {}};
 SysIO button = initSysIO(SIO_PORT0, &bt, press);
 ToneGenerator tg = {initObject(),initCallBlock(), 500, true, 5, FALSE, USEC(100), false,TRUE}; // 500 USEC 650USEC 931USEC
@@ -147,6 +162,37 @@ LED led = {initObject(), 120, TRUE};
 
 ///////////////////////////////////////////////////////////
 // app
+void newrec(App *self, int unused) {
+    CANMsg msg;
+    CAN_RECEIVE(&can0, &msg);
+    char tempBuffer[50];
+
+    sprintf(tempBuffer, "receive a message from node: %d\n", msg.nodeId);
+    SCI_WRITE(&sci0, tempBuffer);
+    if (msg.buff[RECEIVER] != 255 && msg.buff[RECEIVER] != self->Id) {
+        return;
+    }
+    
+    int op = msg.buff[OPCODE];
+    switch (op)
+    {
+    case 0:
+        self->numOfNodes++;
+        SCI_WRITE(&sci0, "nodes: 2\n");
+        break;
+    case 1:
+        ASYNC(&mp, startDS, 0);
+        SCI_WRITE(&sci0, "ready to start \n");
+        break;
+    case 4:
+        ASYNC(&mp, play1Note, msg.buff[2]);
+        sprintf(tempBuffer, "play1note index:%d\n", msg.buff[2]);
+        SCI_WRITE(&sci0, tempBuffer);
+        break;
+    default:
+        break;
+    }
+}
 void receiver(App *self, int unused) {
     CANMsg msg;
     CAN_RECEIVE(&can0, &msg);
@@ -313,13 +359,44 @@ void reader(App *self, int c) {
             SCI_WRITE(&sci0, "Change to conductoer mod\n");
         }
         break;
+    case 'B':   
+        pitchSelfRPC(self->Id);
+        SCI_WRITE(&sci0, "pitch myself\n");
+        break;
+    case 'P': // conductor plays the first note and send message to other nodes.
+        SYNC(&mp, startDS, 0);
+        startRPC(self->Id);
+        ASYNC(&mp, play1Note, 0);
+        break;
     default:
         break;
     }
    
 }
 
+void startRPC(int nodeId) {
+    CANMsg msg;
+    msg.nodeId = nodeId;
+    msg.length = 7;
+    msg.buff[OPCODE] = 1; 
+    msg.buff[RECEIVER] = 255; // broadcast
 
+    CAN_SEND(&can0, &msg);
+    return;
+}
+void pitchSelfRPC(int nodeId) {
+    CANMsg msg;
+    msg.nodeId = nodeId;
+    msg.length = 7;
+    msg.buff[OPCODE] = 0; // claim myself
+    msg.buff[RECEIVER] = 255;
+    CAN_SEND(&can0, &msg);
+    return;
+
+}
+int getId(App* self, int c) {
+    return self->Id;
+}
 void startApp(App *self, int arg) {
     
     SCI_INIT(&sci0); 
@@ -482,6 +559,33 @@ void play(MusicPlayer* self, int c) {
     SEND(MSEC((int)60000 / self->tempo * tempoFactor), USEC(100), self, play, 0);
 }
 
+/* */
+void play1Note(MusicPlayer* self, int noteIndex){
+    int frequency_index;
+    int period;
+    double tempoFactor;
+    SCI_WRITE(&sci0, "want to \n");
+    if (!self->start) {
+        return;
+    }
+    SCI_WRITE(&sci0, "played");
+    if (tempos[noteIndex] == 'b') {
+        tempoFactor = 2.0;
+    } else if (tempos[noteIndex] == 'c'){
+        tempoFactor = 0.5;
+    } else {
+        tempoFactor = 1.0;
+    }
+    SYNC(&tg, muteGap, 0);
+    frequency_index = frequency_indices[noteIndex] + self->key;
+    period = periods[frequency_index+10];
+    SYNC(&tg, changeTone, period);
+    ASYNC(&tg, startTG, 0);
+    ASYNC(&tg, tick, 0);
+    AFTER(MSEC(50), &tg, unMuteGap, 0);
+    SEND(MSEC((int)60000 / self->tempo * tempoFactor), USEC(100), self, stopAndSend, noteIndex);
+
+}
 void changeKey(MusicPlayer* self, int c) {
     keybool = false;
     self->key = c;
@@ -518,7 +622,39 @@ void stop(MusicPlayer* self, int c) {
 
 }
 
+void startDS(MusicPlayer* self, int c) {
+    self->start = TRUE;
+    ASYNC(&led, startled, 0);
+    ASYNC(&led, blink, 0); 
+    
 
+    return;
+
+}
+void stopAndSend(MusicPlayer* self, int noteIndex) {
+    int receiverID;
+    ASYNC(&tg, stopTG, 0);
+    receiverID = (SYNC(&app, getId, 0) + 1) % 2;   // test 2  /// 3
+    play1NoteRPC(noteIndex+1, receiverID);
+    SCI_WRITE(&sci0, "stopMp and sent instruction to next processor \n");
+
+}
+
+void play1NoteRPC(int noteIndex, int receiverID) {
+    CANMsg msg;
+    if (noteIndex == 32){
+        noteIndex = 0;
+    }
+    
+    msg.nodeId = SYNC(&app, getId, 0);
+    msg.length = 7;
+    msg.buff[OPCODE] = 4; // claim myself
+    msg.buff[RECEIVER] = receiverID;
+    msg.buff[2] = noteIndex;
+    CAN_SEND(&can0, &msg);
+    SCI_WRITE(&sci0, "rpc\n");
+    return;
+}
 
 
 // Button
